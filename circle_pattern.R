ae5fe9de1afba4410d40879d7e5f06d744f7d98f
konstantinabi <geom14015@geo.aegean.gr>
2015-09-18 13:17:50 +0300

    more

--- circle_pattern.R
+++ circle_pattern.R
@@ -11,7 +11,7 @@ library(sp)
 point=SpatialPoints(cbind(26.24,39.24))
 
 #' Ορισμός εύρους τιμών για τις ζώνες που θα χρησιμοποιηθούν
-zones=seq(10,15,5)
+zones=seq(1,2,0.01)
 
 #' Δημιουργία κενής λίστας για την μετέπειτα αποθήκευση των στοιχείων του μοτίβου. Ορισμός δείκτη/βήματος
 buffers=list()
@@ -19,21 +19,18 @@ index=1
 
 #' Δημιουργία χωρικών ζωνών.
 for (i in zones){
-  buffes[index]=gBuffer(point, width=i)
+  buffers[index]=gBuffer(point,byid=TRUE, width=i)
   index=index+1
 }
+polygons= lapply(buffers ,function(x) `@`(x ,"polygons"))
+polygons_ind=lapply(unlist(polygons) , function(y) `@`(y,"Polygons"))
+sppolobject=SpatialPolygons(list(Polygons(unlist(polygons_ind) ,ID = 1)))
+
+plot(sppolobject)
+class(sppolobject)
 
 
+#' sppolygondf = SpatialPolygonsDataFrame(sppolobject,dt.f)
+#' writeOGR(sppolygondf, "path",layer="concentric", driver="ESRI Shapefile", overwrite_layer = T)
 
-
-
-
-
-
-
-
-
-
-
-
-
+#' plot(sppolygondf)
