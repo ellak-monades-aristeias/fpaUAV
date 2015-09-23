203458a61fd1766b579e5b0a39efafd1831c8b9c
konstantinabi <geom14015@geo.aegean.gr>
2015-09-23 10:28:38 +0300

    need more

--- circle_pattern.R
+++ circle_pattern.R
@@ -11,7 +11,7 @@ library(sp)
 point=SpatialPoints(cbind(26.24,39.24))
 
 #' Ορισμός εύρους τιμών για τις ζώνες που θα χρησιμοποιηθούν
-zones=seq(1,2,0.01)
+zones=seq(1,20,1)
 
 #' Δημιουργία κενής λίστας για την μετέπειτα αποθήκευση των στοιχείων του μοτίβου. Ορισμός δείκτη/βήματος
 buffers=list()
@@ -22,13 +22,31 @@ for (i in zones){
   buffers[index]=gBuffer(point,byid=TRUE, width=i)
   index=index+1
 }
-polygons= lapply(buffers ,function(x) `@`(x ,"polygons"))
-polygons_ind=lapply(unlist(polygons) , function(y) `@`(y,"Polygons"))
-sppolobject=SpatialPolygons(list(Polygons(unlist(polygons_ind) ,ID = 1)))
 
-plot(sppolobject)
-class(sppolobject)
+IDs <- sapply(buffers, function(x)slot(slot(x, "polygons")[[1]], "ID"))
+length(unique(IDs)) == length(buffers)
+spols <- SpatialPolygons(lapply(1:length(buffers), function(i) {
+  Pols <- slot(buffers[[i]], "polygons")[[1]]
+  slot(Pols, "ID") <- as.character(i)
+  Pols
+}))
 
+IDsunique <- sapply(buffers, function(x) slot(slot(x, "polygons")[[1]], "ID"))
+length(unique(IDsunique)) == length(buffers)
+
+Spols2 <- SpatialPolygons(lapply(buffers,function(x) slot(x, "polygons")[[1]]))
+
+
+
+#polygons= lapply(buffers ,function(x) `@`(x ,"polygons"))
+#polygons_ind=lapply(unlist(polygons) , function(y) `@`(y,"Polygons"))
+#sppolobject=SpatialPolygons(list(Polygons(unlist(polygons_ind) ,ID = 1:20)))
+
+##class(sppolobject)
+
+
+#splines <- as(sppolobject, "SpatialLines")
+#plot(splines)
 
 #' sppolygondf = SpatialPolygonsDataFrame(sppolobject,dt.f)
 #' writeOGR(sppolygondf, "path",layer="concentric", driver="ESRI Shapefile", overwrite_layer = T)
